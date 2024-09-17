dab2b21e07467e5c63d1ff460236084ed7fe5f80
Park <147170724+pjjongho@users.noreply.github.com>
2024-09-17 23:48:27 +0900

    Title: 장학금, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240917/장학금/scholarship.py
+++ 240917/장학금/scholarship.py
@@ -1,10 +1,10 @@
-arr = int(input().split())
-m=arr[0]
-f=arr[1]
+arr = input().split()
+m=int(arr[0])
+f=int(arr[1])
 
-if m>=90 and f >=95:
+if m>=90 and f>=95:
     print("100000")
-elif m>=90 and f >=90:
+elif m>=90 and f>=90:
     print("50000")
 else:
     print("0")
