b68dd6229849e16a68878898de94bb0710f4a28e
Park <147170724+pjjongho@users.noreply.github.com>
2024-09-17 23:45:26 +0900

    Title: 장학금, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240917/장학금/scholarship.py
+++ 240917/장학금/scholarship.py
@@ -1,9 +1,10 @@
-a, b = map(int,input().split())
+arr = int(input().split())
+m=arr[0]
+f=arr[1]
 
-if a>=90:
-    if b>=90:
-        print(50000)
-    if b>=95:
-        print(100000)
-if a<90:
-    print(0)
+if m>=90 and f >=95:
+    print("100000")
+elif m>=90 and f >=90:
+    print("500000")
+else:
+    print("0")
