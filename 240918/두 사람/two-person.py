b1e02c6158c91b4ba51aa02ed6628f4d006e9245
Park <147170724+pjjongho@users.noreply.github.com>
2024-09-18 14:54:52 +0900

    Title: 두 사람, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240918/두 사람/two-person.py
+++ 240918/두 사람/two-person.py
@@ -1,7 +1,7 @@
 a = input().split()
 b = input().split()
 
-if (a[0]>='19' or b[0]>='19') and (a[1]=='M' or b[1]=='M'):
+if (a[0]>='19' and a[1]=='M') or (b[0]>='19' and b[1]=='M'):
     print(1)
 else:
     print(0)
