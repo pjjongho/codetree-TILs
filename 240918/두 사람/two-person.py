1461c1f558d1422711770b9c1f53c24e8077dd19
Park <147170724+pjjongho@users.noreply.github.com>
2024-09-18 15:13:30 +0900

    Title: 두 사람, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240918/두 사람/two-person.py
+++ 240918/두 사람/two-person.py
@@ -1,7 +1,10 @@
 a = input().split()
 b = input().split()
 
-if (a[0]>='19' or b[0]=='19') and (a[1]=='M' or b[1]=='M'):
+a_age = int(a[0])
+b_age = int(b[0])
+
+if (a_age >= 19 and a[1] == 'M') or (b_age >= 19 and b[1] == 'M'):
     print(1)
 else:
     print(0)
