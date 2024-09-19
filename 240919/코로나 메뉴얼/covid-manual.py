0c0650e67645f63027722f39dac9972c80143fff
Park <147170724+pjjongho@users.noreply.github.com>
2024-09-19 12:59:43 +0900

    Title: 코로나 메뉴얼, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240919/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,24 @@
+a = input().split()
+a_g = a[0]
+a_t = int(a[1])
+
+b = input().split()
+b_g = b[0]
+b_t = int(b[1])
+
+c = input().split()
+c_g = c[0]
+c_t = int(c[1])
+
+if a_g=='Y' and a_t>=37:
+    if b_g=='Y' and b_t>=37 or c_g=='Y' and c_t>=37:
+        print("E")
+    else:
+        print('N')
+elif b_g=='Y' and b_t>37:
+    if c_g=='Y' and c_t>=37:
+        print('E')
+    else:
+        print('N')
+else:
+    print('N')
