56f7e63a7775e7a2954a7d1a6ebb1364448f6449
Park <147170724+pjjongho@users.noreply.github.com>
2024-09-21 16:42:43 +0900

    Title: 홀수만 더하기, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240921/홀수만 더하기/add-only-odd-numbers.py
+++ 240921/홀수만 더하기/add-only-odd-numbers.py
@@ -1,6 +1,7 @@
+n = int(input())
 sum_n = 0
 for i in range(1,n+1):
-    n = int(input())
-    if n%2==1 and n%3==0:
-        sum_n += i
+    a = int(input())
+    if a%2==1 and a%3==0:
+        sum_n += a
 print(sum_n)
