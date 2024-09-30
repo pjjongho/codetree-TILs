92b5d3ba0a1af65afedf00845f4c4bf8cbe13d47
Park <147170724+pjjongho@users.noreply.github.com>
2024-09-30 14:00:35 +0900

    Title: 별 그리기, Time: 167ms, Memory: 27MB, Status: Passed - CodeTree

--- 240930/별 그리기/star-drawing.py
+++ 240930/별 그리기/star-drawing.py
@@ -1,17 +1,15 @@
 n = int(input())
 
 for i in range(n):
-    for _ in range(n-(i+1)):
+    for j in range(n-(i+1)):
         print(' ',end='')
-    
-    for _ in range((i*2)+1):
-        print('*',end='')
+    for j in range((2*i)+1):
+        print('*', end='')
     print()
 
-for i in range(0,n-1):
-    for _ in range(i+1):
+for i in range(0,n):
+    for j in range(i+1):
         print(' ',end='')
-
-    for _ in range((2*n)-(2*i)-n):
-        print('*',end='')
+    for j in range((2*n)-(2*i)-3):
+        print('*', end='')
     print()
