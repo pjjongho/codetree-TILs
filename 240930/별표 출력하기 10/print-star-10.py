1035eaf493b231a0edc7ac20f3aedcdd3e8801ff
Park <147170724+pjjongho@users.noreply.github.com>
2024-09-30 15:06:57 +0900

    Title: 별표 출력하기 10, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240930/별표 출력하기 10/print-star-10.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+odd=0
+even=n+1
+
+for i in range(1, n*2+1):
+    if i%2==1:
+        odd+=1
+        for j in range(odd):
+            print('*',end=' ')
+        print()
+    else:
+        even -=1
+        for j in range(even):
+            print('*', end=' ')
+        print()
