371ad5590f94ee642ec5223961bf10c2fe1a03a0
Park <147170724+pjjongho@users.noreply.github.com>
2024-10-05 14:59:48 +0900

    Title: 배열의 특정 위치 찾기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241005/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 241005/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -1,12 +1,17 @@
 arr = list(map(int,input().split()))
 cnt = 0
+filtered_arr1 = arr[1::2]
+filtered_arr2 = arr[2::3]
 sum_val = 0
 sum_val2 = 0
 
-for i in arr:
+for i in filtered_arr1:
     if i%2==0:
         sum_val += i
+
+for i in filtered_arr2:
     if i%3==0:
         sum_val2 +=i
         cnt += 1
+
 print(f'{sum_val} {(sum_val2/cnt):.1f}')
