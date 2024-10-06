97832b1787a7afc7e8381c6ca47e301b2be54586
Park <147170724+pjjongho@users.noreply.github.com>
2024-10-06 17:11:06 +0900

    Title: 100 도달하기, Time: 158ms, Memory: 24MB, Status: Passed - CodeTree

--- 241006/100 도달하기/reach-100.py
+++ 241006/100 도달하기/reach-100.py
@@ -1,10 +1,16 @@
 n = int(input())
-arr = [1,n]
+arr = [1, n]
 
 while True:
-    for i in range(0,len(arr)):
-        arr.append(arr[len(arr) - 1] + arr[len(arr) - 2])
-
-    if arr[len(arr) - 1] >= 100:
+    # 새로 추가될 값을 미리 계산
+    next_value = arr[-1] + arr[-2]
+    
+    # 100을 넘는 경우 그 값을 추가하고 종료
+    if next_value > 100:
+        arr.append(next_value)
         break
+    
+    arr.append(next_value)
+
+# 결과 출력
 print(*arr)
