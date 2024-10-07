a0b24a76a9463cc05ac32c90ce06dd26bf2a4b66
Park <147170724+pjjongho@users.noreply.github.com>
2024-10-07 15:43:42 +0900

    Title: 배열 놀이, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 241007/배열 놀이/play-with-array.py
+++ 241007/배열 놀이/play-with-array.py
@@ -1,19 +1,24 @@
 n, q = map(int, input().split())
 
-dnjsth = list(map(int, input().split()))
+elim = list(map(int, input().split()))
 
 for _ in range(q):
     lst = list(map(int, input().split()))
     if lst[0] == 1:
-        print(dnjsth[lst[1]-1])
+        # a번째 원소 출력
+        print(elim[lst[1]-1])
+        
     elif lst[0] == 2:
-        if lst[1] in dnjsth:
-            for j in dnjsth:
-                if j == lst[1]:
-                    print(dnjsth.index(j)+1)
-                    break
-        else:
-            print('0')
+        # 값이 b인 원소가 몇 번째 원소인지 찾기
+        found = False
+        for i in range(len(elim)):
+            if elim[i] == lst[1]:
+                print(i + 1)
+                found = True
+                break
+        if not found:
+            print(0)
+            
     elif lst[0] == 3:
-        for i in range(lst[1]-1, lst[2]):
-            print(dnjsth[i], end=' ')
+        # s번째 원소부터 e번째 원소까지 출력 (공백으로 구분)
+        print(' '.join(map(str, elim[lst[1]-1:lst[2]])))
