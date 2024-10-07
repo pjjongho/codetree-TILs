60081d4a4b9dca263c358fd4955449e009385123
Park <147170724+pjjongho@users.noreply.github.com>
2024-10-07 11:42:50 +0900

    Title: 전항의 두 배, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241007/전항의 두 배/twice-the-previous.py
@@ -0,0 +1,9 @@
+a,b = list(map(int,input().split()))
+
+for i in range(10):
+    if i%2==0:
+        print(a, end=' ')
+        a = b + 2*a
+    else:
+        print(b, end=' ')
+        b = a + 2*b
