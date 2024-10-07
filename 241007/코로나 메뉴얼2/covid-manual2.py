b05023ff8dd7d40016e0db12aa5dedd7b2f30a2e
Park <147170724+pjjongho@users.noreply.github.com>
2024-10-07 14:28:29 +0900

    Title: 코로나 메뉴얼2, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 241007/코로나 메뉴얼2/covid-manual2.py
+++ 241007/코로나 메뉴얼2/covid-manual2.py
@@ -1,18 +1,18 @@
-che =[0]*4
+temp =[0]*4
 
 for i in range(3):
     a,b=input().split()
     b=int(b)
     if a =='Y'and b>=37:
-        che[0]+=1
+        temp[0]+=1
     elif a=='N' and b>=37:
-        che[1]+=1
+        temp[1]+=1
     elif a=='Y'and b<37:
-        che[2]+=1
+        temp[2]+=1
     elif a=='N'and b<37:
-        che[3]+=1
-for j in che:
+        temp[3]+=1
+for j in temp:
     print(j,end=' ')
 
-if che[0]>=2:
+if temp[0]>=2:
     print('E')
