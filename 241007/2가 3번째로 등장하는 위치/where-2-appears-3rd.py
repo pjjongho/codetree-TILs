471fdb099aabfce933c7ecc0a2bf0cc824b18662
Park <147170724+pjjongho@users.noreply.github.com>
2024-10-07 15:59:28 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 241007/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 241007/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -1,10 +1,9 @@
-n = int(input())
-arr = list(map(int,input().split()))
-cnt = 0
-
-for i in range(n):
-    if arr[i]==2:
+n=int(input())
+arr = list(map(int, input().split()))
+cnt, result =0, 0
+for idx, el in enumerate(arr):
+    if el == 2:
         cnt+=1
-    if cnt == 3:
-        break
-print(arr[i]+1)
+        if cnt == 3:
+            result = idx
+print(result+1)
