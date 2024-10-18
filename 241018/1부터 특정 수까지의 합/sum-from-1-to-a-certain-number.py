ec1ecc66200dd06afe534afa3c58015c64735565
Park <147170724+pjjongho@users.noreply.github.com>
2024-10-18 17:21:55 +0900

    Title: 1부터 특정 수까지의 합, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 241018/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.py
+++ 241018/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.py
@@ -1,8 +1,10 @@
+import math
+
 def sum_num(n):
     num = 0
     for i in range(1,n+1):
         num += i
-    print(f'{(num/10):.0f}')
+    print(f'{math.floor(num/10)}')
 
 n = int(input())
 sum_num(n)
