4dab56b629f19cade33c51236bc2356b71915c5d
Park <147170724+pjjongho@users.noreply.github.com>
2024-10-18 15:11:58 +0900

    Title: 최소공배수 구하기, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 241018/최소공배수 구하기/find-the-least-common-multiple.py
+++ 241018/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,10 +1,7 @@
-n, m = map(int,input().split())
+import math
 
-def max_num(n,m):
-    list = []
-    for i in range(1,101):
-        if i%n==0 and i%m==0:
-            list.append(i)
-    print(min(list))
-        
-max_num(n,m)
+def lcm(n,m):
+    return (n*m) // math.gcd(n,m)
+
+n, m = map(int,input().split())
+print(lcm(n,m))
